1a1041ac4c5000dc167b2ec55e4ad0e05e419ff0
DEV-Rithik <rithikbasava6@gmail.com>
2026-02-04 10:52:53 +0530

    strings without stringspecifier

--- 1.py
+++ 1.py
@@ -1,2 +1,15 @@
 a = input("enter your name:")
 print(a)
+
+
+a = int(input("enter number 1:"))
+b = int(input("enter number 2:"))
+print(a+b)
+
+
+
+a = int(input("enter a number:"))
+if a%2 == 0:
+    print("entered number is even")
+else:
+    print("entered number is odd")
--- relational.py
+++ relational.py
@@ -1,4 +1,4 @@
 a = 9
 b = 5
 
-print(a "<" b,a<b)
+print(a,b,a<b)
