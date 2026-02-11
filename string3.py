46ae19c007e7b56a8cb423a389ef2a1578a7010b
DEV-Rithik <rithikbasava6@gmail.com>
2026-02-11 11:03:35 +0530

    ......

---
+++ string3.py
@@ -0,0 +1,7 @@
+while i<2:
+   s = int(input("enter your number :"))
+print(s)
+if s[0] > s[1] :
+    print("greater")
+else:
+    print("lower")
---
+++ test.py
@@ -0,0 +1,10 @@
+# num = 5
+# base = 2
+# print("the number {num} and base {base}")
+
+# for _ in range(0,3):
+#     print(_)
+
+
+text = "MechaNical Robotics AI"
+print(text.capitalize())
