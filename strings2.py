e4d02c6b94f7ae92517cfde6d1ad25ed6e82ab57
DEV-Rithik <rithikbasava6@gmail.com>
2026-02-21 09:26:23 +0530

    ......

---
+++ strings2.py
@@ -0,0 +1,5 @@
+
+only_letters = "HelloWorld"
+only_digits = "12345"
+print("'HelloWorld'.isalpha() ->", only_letters.isalpha())
+print("'12345'.isdigit() ->", only_digits.isdigit())
--- test.py
+++ test.py
@@ -1,9 +1,9 @@
-# num = 5
-# base = 2
-# print("the number {num} and base {base}")
+num = 5
+base = 2
+print("the number {num} and base {base}")
 
-# for _ in range(0,3):
-#     print(_)
+for _ in range(0,3):
+     print(_)
 
 
 text = "MechaNical Robotics AI"
